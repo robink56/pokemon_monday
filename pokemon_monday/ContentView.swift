07be004cfc4df8d99a829c314aa39b7632cac4fd
Robin Khatri <rkhatri@realists.de>
2026-06-29 10:26:57 +0200

    Initial Commit

---
+++ pokemon_monday/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pokemon_monday
+//
+//  Created by rkhatri on 29.06.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pokemon_monday/pokemon_mondayApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokemon_mondayApp.swift
+//  pokemon_monday
+//
+//  Created by rkhatri on 29.06.26.
+//
+
+import SwiftUI
+
+@main
+struct pokemon_mondayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
